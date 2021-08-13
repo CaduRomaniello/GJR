55d17e833039cfd4e82af1c9d7a659b299c2b5f8
Daniel-M30 <daniel_monteiro30@hotmail.com>
2021-08-13 20:46:48 -0300

    Documentando codigo

--- Runtime/src/includes/airplane.h
+++ Runtime/src/includes/airplane.h
@@ -6,37 +6,101 @@
 
 using namespace std;
 
+//! Class Airplane
+/*
+ * This Class represents an airplane in the General Systems Theory implemented in this code.
+*/
 class Airplane{
 
     public:
 
+        /*!
+            This is the default destructor for the Airplane Class.
+        */
         virtual ~Airplane(){}
 
+        /*!
+            Returns the id atttribute in the Airplane Class.
+            \return int - the content id attribute.
+        */
         virtual int getId() const = 0;
+
+        /*!
+            Sets the id attribute in the Airplane Class.
+            \param id which will be set to the current Airplane.
+        */
         virtual void setId(int id) = 0;
 
-
+        /*!
+            Returns the model atttribute in the Airplane Class.
+            \return string - the content model attribute.
+        */
         virtual string getModel() const = 0;
+
+        /*!
+            Sets the model attribute in the Airplane Class.
+            \param model which will be set to the current Airplane.
+        */
         virtual void setModel(string model) = 0;
 
-
+        /*!
+            Returns the manufacturer atttribute in the Airplane Class.
+            \return string - the content manufacturer attribute.
+        */
         virtual string getManufacturer() const = 0;
+
+        /*!
+            Sets the manufacturer attribute in the Airplane Class.
+            \param manufacturer which will be set to the current Airplane.
+        */
         virtual void setManufacturer(string manufacturer) = 0;
 
-
+        /*!
+            Returns the registration atttribute in the Airplane Class.
+            \return string - the content registration attribute.
+        */
         virtual string getRegistration() const = 0;
+
+        /*!
+            Sets the registration attribute in the Airplane Class.
+            \param registration which will be set to the current Airplane.
+        */
         virtual void setRegistration(string registration) = 0;
 
-
+        /*!
+            Returns the pilot atttribute in the Airplane Class.
+            \return string - the content pilot attribute.
+        */
         virtual string getPilot() const = 0;
+
+        /*!
+            Sets the pilot attribute in the Airplane Class.
+            \param pilot which will be set to the current Airplane.
+        */
         virtual void setPilot(string pilot) = 0;
 
-
+        /*!
+            Returns the copilot atttribute in the Airplane Class.
+            \return string - the content copilot attribute.
+        */
         virtual string getCopilot() const = 0;
+
+        /*!
+            Sets the copilot attribute in the Airplane Class.
+            \param copilot which will be set to the current Airplane.
+        */
         virtual void setCopilot(string copilot) = 0;
 
-
+        /*!
+            Returns the capacity atttribute in the Airplane Class.
+            \return int - the content capacity attribute.
+        */
         virtual int getCapacity() const = 0;
+
+        /*!
+            Sets the capacity attribute in the Airplane Class.
+            \param capacity which will be set to the current Airplane.
+        */
         virtual void setCapacity(int capacity) = 0;
 
 };
--- Runtime/src/includes/flight.h
+++ Runtime/src/includes/flight.h
@@ -6,37 +6,100 @@
 
 using namespace std;
 
+//! Class Flight
+/**
+* This Class represents a flight in the General Systems Theory implemented in this code.
+*/
 class Flight{
 
     public:
-
+        /*!
+            This is the default destructor for the Flight Class.
+        */
         virtual ~Flight(){}
 
+        /*!
+            Returns the id attribute in the Flight Class.
+            \return int - the content id attribute.  
+        */
         virtual int getId() const = 0;
+
+        /*!
+            Sets the id attribute in the Flight Class.
+            \param flightIdentifier which will be set to the current Flight.
+        */
         virtual void setId(int flightIdentifier) = 0;
 
-
+        /*!
+            Returns the time attribute in the Flight Class.
+            \return string - the content time attribute.  
+        */
         virtual string getTime() const = 0;
+
+        /*!
+            Sets the time attribute in the Flight Class.
+            \param time which will be set to the current Flight.
+        */
         virtual void setTime(string time) = 0;
 
-
+        /*!
+            Returns the date attribute in the Flight Class.
+            \return string - the content date attribute.  
+        */
         virtual string getDate() const = 0;
+
+        /*!
+            Sets the date attribute in the Flight Class.
+            \param date which will be set to the current Flight.
+        */
         virtual void setDate(string date) = 0;
 
-
+        /*!
+            Returns the idAirplane attribute in the Flight Class.
+            \return int - the content idAirplane attribute.  
+        */
         virtual int getIdAirplane() const = 0;
+
+        /*!
+            Sets the idAirplane attribute in the Flight Class.
+            \param idAirplane which will be set to the current Flight.
+        */
         virtual void setIdAirplane(int idAirplane) = 0;
 
-
+        /*!
+            Returns the origin attribute in the Flight Class.
+            \return string - the content origin attribute.  
+        */
         virtual string getOrigin() const = 0;
+
+        /*!
+            Sets the origin attribute in the Flight Class.
+            \param origin which will be set to the current Flight.
+        */
         virtual void setOrigin(string origin) = 0;
 
-
+        /*!
+            Returns the destiny attribute in the Flight Class.
+            \return string - the content destiny attribute.  
+        */
         virtual string getDestiny() const = 0;
+
+        /*!
+            Sets the destiny attribute in the Flight Class.
+            \param destiny which will be set to the current Flight.
+        */
         virtual void setDestiny(string destiny) = 0;
 
-
+        /*!
+            Returns the numberOfAvailableSeats attribute in the Flight Class.
+            \return int - the content numberOfAvailableSeats attribute.  
+        */
         virtual int getNumberOfAvailableSeats() const = 0;
+
+        /*!
+            Sets the numberOfAvailableSeats attribute in the Flight Class.
+            \param availableSeats which will be set to the current Flight.
+        */
         virtual void setNumberOfAvailableSeats(int availableSeats) = 0;
 
 };
